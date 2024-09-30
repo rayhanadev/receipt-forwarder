410fe2c367672e382ab14c8688f7efa9c82bd2cc
ray <me@rayhanadev.com>
2024-09-30 03:37:23 -0400

    feat: display emails in discord

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,62 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { EmailMessage } from "cloudflare:email";
+import PostalMime from "postal-mime";
+
+async function streamToArrayBuffer(stream: ReadableStream, streamSize: number) {
+  let result = new Uint8Array(streamSize);
+  let bytesRead = 0;
+  const reader = stream.getReader();
+  while (true) {
+    const { done, value } = await reader.read();
+    if (done) {
+      break;
+    }
+    result.set(value, bytesRead);
+    bytesRead += value.length;
+  }
+  return result;
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
+  async email(event, env) {
+    console.log("Received email event");
+
+    const rawEmail = await streamToArrayBuffer(event.raw, event.rawSize);
+    console.log("Converted email to buffer");
+
+    const parser = new PostalMime();
+    const contents = await parser.parse(rawEmail);
+    const html = contents.html ?? `<pre>${contents.text}</pre>`;
+    console.log("Parsed email");
+
+    const key = `email-${new Date().getTime()}.html`;
+    await env.R2.put(key, html);
+    console.log("Uploaded email to KV");
+
+    const response = await fetch(env.DISCORD_WEBHOOK_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        embeds: [
+          {
+            title: "New Receipt Received",
+            description: `A new email has been received from ${event.from}.`,
+            image: {
+              url: `https://phack-webrender.fly.dev/api/render?fullPage=1&url=https://receipts.c0w.sh/${key}`,
+            },
+          },
+        ],
+      }),
+    });
+    console.log("Sent message to Discord");
+
+    if (!response.ok) {
+      throw new Error(
+        `Failed to send message to Discord: ${response.statusText}`
+      );
+    }
+
+    console.log("Email event complete");
+  },
 } satisfies ExportedHandler<Env>;
